f096bbcbea14f751f7c69acfc0e529dad692a5e9
alextula26 <alex_marc86@mail.ru>
2022-12-03 00:18:03 +0300

    Добавил урл к mongodb в .env

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -3,8 +3,13 @@ import * as dotenv from 'dotenv'
 import { BlogType, PostType } from '../types'
 dotenv.config()
 
-// const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017'
-const client = new MongoClient('mongodb+srv://alextula86:marchuk2008@cluster0.mms9f2q.mongodb.net/?retryWrites=true&w=majority')
+const mongoUri = process.env.MONGO_ATLAS_URI || process.env.MONGO_URI
+
+if (!mongoUri) {
+  throw new Error('Url doesnt found')
+}
+
+const client = new MongoClient(mongoUri)
 const db = client.db('bloggers')
 
 export const blogCollection = db.collection<BlogType>('blogs')
