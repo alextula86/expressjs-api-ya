9bb6d7c393d3c584f1488116aa20c914d9791e43
alextula26 <alex_marc86@mail.ru>
2022-12-21 09:14:21 +0300

    Update testingRepository

--- src/repositories/testing/testing-db-repository.ts
+++ src/repositories/testing/testing-db-repository.ts
@@ -1,10 +1,11 @@
-import { blogCollection, postCollection, userCollection } from '../../repositories/db'
-import { RepositoryTestingType } from '../../types/service'
+import { blogCollection, postCollection, commentCollection, userCollection } from '../../repositories/db'
+import { RepositoryTestingType } from '../../types'
 
 export const testingRepository: RepositoryTestingType = {
   deleteAll: async () => {
-    await postCollection.deleteMany({})
     await blogCollection.deleteMany({})
+    await postCollection.deleteMany({})
+    await commentCollection.deleteMany({})
     await userCollection.deleteMany({})
 
     return true
