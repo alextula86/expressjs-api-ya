1b63ebcf529295538ce51c356d989e790149750a
alextula26 <alex_marc86@mail.ru>
2023-01-10 18:28:02 +0300

    fix

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -4,11 +4,11 @@ import { UserType } from '../types'
 
 export const jwtService = {
   async createAccessToken (userId: string) {
-    const accessToken = jwt.sign({ userId }, settings.ACCESS_TOKEN_SECRET, { expiresIn: '10d' })
+    const accessToken = jwt.sign({ userId }, settings.ACCESS_TOKEN_SECRET, { expiresIn: '10s' })
     return { accessToken }
   },
   async createRefreshToken (userId: string) {
-    const refreshToken = jwt.sign({ userId }, settings.REFRESH_TOKEN_SECRET, { expiresIn: '20d' })
+    const refreshToken = jwt.sign({ userId }, settings.REFRESH_TOKEN_SECRET, { expiresIn: '20s' })
     return { refreshToken }
   },   
   async getUserIdByAccessToken (token: string) {
